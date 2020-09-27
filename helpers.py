093aa962b3ba51604ea92b35da136fc9c5befbad
bluewaree <belkihelmehdi2@gmail.com>
2020-09-27 19:05:17 +0100

    feat: add text characters count helper

--- helpers.py
+++ helpers.py
@@ -1,5 +1,7 @@
 import re
 
+FIRST_ASCII_LETTER = 97
+
 
 def count_text_length(text):
     length_with_space = len(text)
@@ -10,4 +12,17 @@ def count_text_length(text):
 def count_text_words(text):
     text_without_extra_spaces = re.sub(" {2,} ", " ", text).strip()
     return len(text_without_extra_spaces.split(" "))
+
+def count_text_characters(text):
+    letters_count = [0] * 26
+    for letter in text:
+        if letter.isalpha():
+            letters_count[ord(letter) - FIRST_ASCII_LETTER] += 1
+
+    letters_count_data = []
+    for i in range(26):
+        if letters_count[i] > 0:
+            letters_count_data.append({chr(i+FIRST_ASCII_LETTER): letters_count[i] })
+
+    return letters_count_data
 
--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -13,3 +13,15 @@ def test_text_words_count():
     assert helpers.count_text_words(text) == 2
     assert helpers.count_text_words(text_with_extra_spaces) == 3
 
+
+def test_text_characters_count():
+    text = " a bb ccc dddd zzzzz!"
+    data = helpers.count_text_characters(text)
+
+    assert len(data) == 5
+    assert data[0]["a"] == 1
+    assert data[1]["b"] == 2
+    assert data[2]["c"] == 3
+    assert data[3]["d"] == 4
+    assert data[4]["z"] == 5
+
