904162dff9f83d6f641fdf80984f651950ed4187
bluewaree <belkihelmehdi2@gmail.com>
2020-09-27 18:57:16 +0100

    feat: add text words count helper

--- helpers.py
+++ helpers.py
@@ -1,4 +1,13 @@
+import re
+
+
 def count_text_length(text):
     length_with_space = len(text)
     length_without_space = len(text.replace(" ",""))
     return length_with_space, length_without_space
+
+
+def count_text_words(text):
+    text_without_extra_spaces = re.sub(" {2,} ", " ", text).strip()
+    return len(text_without_extra_spaces.split(" "))
+
--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,6 +1,15 @@
-from helpers import count_text_length
+import helpers
+
 
 def test_text_length_count():
     text = "a b"
-    length_with_space, length_without_space = count_text_length(text)
+    length_with_space, length_without_space = helpers.count_text_length(text)
     assert length_with_space == 3, length_without_space == 2
+
+
+def test_text_words_count():
+    text = "a b"
+    text_with_extra_spaces = "    a b     c    "
+    assert helpers.count_text_words(text) == 2
+    assert helpers.count_text_words(text_with_extra_spaces) == 3
+
